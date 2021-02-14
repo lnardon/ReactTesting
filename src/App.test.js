6b40668e9390dc5abebeaa6832a630d1f1eb132e
lnardon <lucaslnardon@gmail.com>
2021-02-14 08:17:00 -0300

    :tada:First commit

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders learn react link", () => {
+  const wraper = shallow(<App />);
+});
