b1e4c9c21ffd60822e7d4f635f4851c0a1982254
lnardon <lucaslnardon@gmail.com>
2021-02-15 06:28:26 -0300

    ✨ Create Congrats component based on the tests.

--- src/components/Congrats/index.js
+++ src/components/Congrats/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Congrats({ sucess }) {
-  return <div />;
+function Congrats({ success, message }) {
+  return (
+    <div data-test="componentCongrats">
+      <h2 data-test="componentCongratsMessage">{message}</h2>
+    </div>
+  );
 }
 
 export default Congrats;
