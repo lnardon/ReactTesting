dffb4837c08509d2764740ad382a869bda18a75a
lnardon <lucaslnardon@gmail.com>
2021-02-24 22:27:50 -0300

    ✨ Add List component structure code

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function List({ words }) {
-  return <div />;
+function List({ words = [] }) {
+  return (
+    <div data-test="componentList">
+      {words.length > 0 ? (
+        <div data-test="componentListTable">
+          {words.map((word) => {
+            return <h1 key={word}>{word}</h1>;
+          })}
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default List;
